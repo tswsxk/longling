93c38e559bbaa32c28f5109decc4dc6a6eaab55f
tswsxk <tongsw@mail.ustc.edu.cn>
2018-07-26 17:25:27 +0800

    重命名部分函数名

--- longling/framework/ML/universe/fileio/jsonxz.py
+++ longling/framework/ML/universe/fileio/jsonxz.py
@@ -9,14 +9,36 @@ import json
 from tqdm import tqdm
 
 from ..dataIterator import CSVIterator
-from ..dataIterator import JsonxzIterator, JsonxzBatchIterator
 
 from longling.lib.stream import wf_open, wf_close
 
 
-def transform_csv2jsonxz(source, target, label_index=None, label_name=None,
-                         reserved_fields_index=None, reserved_fields_name=None,
-                         **kwargs):
+def load(source, data_key='x', label_key='z'):
+    datas = []
+    labels = []
+    with open(source) as f:
+        for line in tqdm(f):
+            data = json.loads(line)
+            x, z = data[data_key], data[label_key]
+            datas.append(x)
+            labels.append(z)
+    return datas, labels
+
+
+def dump(objs, fp, wrapper=tqdm):
+    for obj in wrapper(objs):
+        print(json.dumps(obj, ensure_ascii=False), file=fp)
+
+
+def verify(filename):
+    with open(filename) as f:
+        for line in tqdm(f):
+            json.loads(line)
+
+
+def csv2jsonxz(source, target, label_index=None, label_name=None,
+               reserved_fields_index=None, reserved_fields_name=None,
+               **kwargs):
 
     if label_index is None and label_name is None:
         raise ValueError("label_index and label_name both are None")
@@ -33,7 +55,7 @@ def transform_csv2jsonxz(source, target, label_index=None, label_name=None,
         label_index = dict(datas.reserved_name_index_pairs)[label_name]
 
     data_key = kwargs.get('data_key', 'x')
-    label_key = kwargs.get('lebel_key', 'z')
+    label_key = kwargs.get('label_key', 'z')
 
     wf = wf_open(target)
     for data in datas:
@@ -46,15 +68,3 @@ def transform_csv2jsonxz(source, target, label_index=None, label_name=None,
         xz = {data_key: x, label_key: z}
         print(json.dumps(xz, ensure_ascii=False), file=wf)
     wf_close(wf)
-
-
-def load_jsonxz(source, data_key='x', label_key='z'):
-    datas = []
-    labels = []
-    with open(source) as f:
-        for line in tqdm(f):
-            data = json.loads(line)
-            x, z = data[data_key], data[label_key]
-            datas.append(x)
-            labels.append(z)
-    return datas, labels
---
+++ longling/framework/db/sql.md
@@ -0,0 +1,10 @@
+## cache
+* 强制类型转换
+CAST(updatetime as bigint)
+* 时间戳转时间
+FROM_UNIXTIME(bigint_num)
+* 生成时间
+date('2017-09-01')
+* 浮点数转长整数
+floor(number / 1000)
+* 非空
