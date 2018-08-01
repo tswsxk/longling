b81a8e0ee53e545a7bd6059cc32957b951f779e0
tswsxk <tongsw@mail.ustc.edu.cn>
2018-08-01 17:22:01 +0800

    添加可视化异常

--- longling/framework/ML/MXnet/viz.py
+++ longling/framework/ML/MXnet/viz.py
@@ -5,6 +5,11 @@ import mxnet as mx
 
 from longling.lib.stream import build_dir
 
+import traceback
+
+class VizError(Exception):
+    pass
+
 
 def form_shape(data_iter):
     return {d[0]: d.shape for d in data_iter.provide_data}
@@ -15,4 +20,7 @@ def plot_network(nn_symbol, save_path="plot/network", shape=None, node_attrs={},
 
     assert save_path
     build_dir(save_path)
-    graph.render(save_path, view=view)
+    try:
+        graph.render(save_path, view=view)
+    except Exception:
+        raise VizError(traceback.format_exc())
