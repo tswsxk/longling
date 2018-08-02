7543f97e74bfea6a3550e5bd91719a682702f19c
tswsxk <tongsw@mail.ustc.edu.cn>
2018-08-02 10:17:15 +0800

    添加残差网络单元

--- longling/framework/ML/MXnet/mx_gluon/gluon_sym/__init__.py
+++ longling/framework/ML/MXnet/mx_gluon/gluon_sym/__init__.py
@@ -6,3 +6,5 @@ from __future__ import absolute_import
 from .nn_loss import *
 from .nn_cell import TextCNN, TransE, highway_cell
 from .nn_sym import PVWeight
+
+from mxnet.gluon.rnn.rnn_cell import ResidualCell
