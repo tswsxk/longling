4a171bc43202c156bc5257bce9907a8e05e444c8
tswsxk <tongsw@mail.ustc.edu.cn>
2018-08-07 14:17:53 +0800

    重设版本号

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
 # coding: utf-8
 # created by tongshiwei on 17-12-17
+import io
+import re
+import os
 from distutils.core import setup
 from setuptools import find_packages
 
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+VERSION = find_version('longling', '__init__.py')
+
+
 setup(
     name='longling',
-    version='0.0.11',
+    version=VERSION,
     author='Sherlock, Shiwei Tong',
     author_email='tongsw@mail.ustc.edu.cn',
     packages=find_packages(
