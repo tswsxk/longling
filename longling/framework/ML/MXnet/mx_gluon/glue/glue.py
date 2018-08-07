21ff5b632b096b8836d06b5df25e6b6dc04d066d
tswsxk <tongsw@mail.ustc.edu.cn>
2018-08-07 15:02:20 +0800

    新增异常处理

--- longling/framework/ML/MXnet/mx_gluon/glue/glue.py
+++ longling/framework/ML/MXnet/mx_gluon/glue/glue.py
@@ -27,16 +27,25 @@ def new_module(module_name, directory=None):
         return name.replace("module_name", module_name).replace("GluonModule", big_module_name)
 
     for root, dirs, files in os.walk(glum_directory):
-        if 'data' + os.sep in root:
+        if 'data' + os.sep in root or '_build' in root or os.path.join('docs', 'source') in root:
+            logger.debug("skip %s-%s" % (root, files))
             continue
         for filename in files:
+            if '.pyc' in filename:
+                logger.debug("skip %s-%s" % (root, filename))
+                continue
             dirname = os.path.abspath(name_replace(root))
             source_file = os.path.abspath(os.path.join(root, filename))
             target_file = os.path.abspath(os.path.join(dirname, name_replace(filename)))
             logger.debug(source_file, '->', target_file)
             with open(source_file, encoding="utf-8") as f, wf_open(target_file) as wf:
-                for line in f:
-                    print(name_replace(line), end="", file=wf)
+                try:
+                    for line in f:
+                        print(name_replace(line), end="", file=wf)
+                except UnicodeDecodeError:
+                    print(source_file, line)
+                    exit(-1)
+                    return False
     return True
 
 
