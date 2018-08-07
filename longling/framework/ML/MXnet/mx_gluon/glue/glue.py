23b84139a11c3f64e4003c7871f052b3e1eba1c4
tongshiwei <tongsw@mail.ustc.edu.cn>
2018-08-07 21:27:26 +0800

    重构，内部测试通过，外部仍有问题

--- longling/framework/ML/MXnet/mx_gluon/glue/glue.py
+++ longling/framework/ML/MXnet/mx_gluon/glue/glue.py
@@ -54,7 +54,7 @@ def new_module(module_name, directory=None):
 if __name__ == '__main__':
     import argparse
 
-    module_name = "SNN"
+    module_name = "RLSTM"
     parser = argparse.ArgumentParser()
     if module_name:
         parser.add_argument("--module_name", default="%s" % module_name,
