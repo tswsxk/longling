cdbedd2516efac7ba4f7ea3185fd51d1718f2d61
tswsxk <tongsw@mail.ustc.edu.cn>
2018-08-07 08:33:28 +0800

    新增顶层导入

--- longling/framework/ML/MXnet/mx_gluon/__init__.py
+++ longling/framework/ML/MXnet/mx_gluon/__init__.py
@@ -1,2 +1,11 @@
 # coding: utf-8
 # created by tongshiwei on 18-2-5
+
+from __future__ import absolute_import
+
+from .glue import glue
+
+from . import gluon_util as util
+from . import gluon_sym as sym
+from . import gluon_module as module
+from . import gluon_toolkit as toolkit
