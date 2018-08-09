93010e0450d94dd1f7fd0ca66ca46486ab4fb151
tongshiwei <tongsw@mail.ustc.edu.cn>
2018-08-09 22:13:45 +0800

    fix some bugs

--- longling/__init__.py
+++ longling/__init__.py
@@ -1,21 +1,21 @@
 # coding: utf-8
 # created by tongshiwei on 17-12-17
 
-# from __future__ import absolute_import
-#
-# from longling.lib import clock
-# from longling.lib import process_pattern
-# from longling.lib import stream
-# from longling.lib import utilog
-#
-# from longling.base import *
-# from longling.lib.stream import *
-#
-# # import longling.framework.ML.NLP as NLP
-# from longling.framework.ML import MXnet
-# from longling.framework.ML import MXnet as mxnet
-# from longling.framework.ML.MXnet import mx_gluon
-# from longling.framework.ML.MXnet import mx_gluon as gluon
+from __future__ import absolute_import
+
+from longling.lib import clock
+from longling.lib import process_pattern
+from longling.lib import stream
+from longling.lib import utilog
+
+from longling.base import *
+from longling.lib.stream import *
+
+# import longling.framework.ML.NLP as NLP
+from longling.framework.ML import MXnet
+from longling.framework.ML import MXnet as mxnet
+from longling.framework.ML.MXnet import mx_gluon
+from longling.framework.ML.MXnet import mx_gluon as gluon
 
 
 __version__ = '0.0.11'
