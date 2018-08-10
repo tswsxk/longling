11b269f8d62bc30d4251dcf42368b523fd78ee95
tongshiwei <tongsw@mail.ustc.edu.cn>
2018-08-10 11:00:11 +0800

    NI

--- longling/__init__.py
+++ longling/__init__.py
@@ -3,13 +3,13 @@
 
 from __future__ import absolute_import
 
+from longling.base import *
+from longling.lib.stream import *
+
 from longling.lib import clock
 from longling.lib import process_pattern
 from longling.lib import stream
 from longling.lib import utilog
-
-from longling.base import *
-from longling.lib.stream import *
 
 # import longling.framework.ML.NLP as NLP
 from longling.framework.ML import MXnet
