866be28d2bf0ef03839e2cab3bbfbb17cc7845a7
tongshiwei <tongsw@mail.ustc.edu.cn>
2018-08-12 10:02:59 +0800

    新增attetionn

--- longling/framework/ML/MXnet/mx_gluon/gluon_sym/__init__.py
+++ longling/framework/ML/MXnet/mx_gluon/gluon_sym/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 
 from .nn_loss import *
 from .nn_cell import TextCNN, TransE, HighwayCell
-from .nn_sym import PVWeight
+from .nn_sym import *
 
 from mxnet.gluon.rnn.rnn_cell import ResidualCell
 
---
+++ longling/framework/ML/MXnet/mx_gluon/gluon_sym/nn_sym/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# create by tongshiwei on 2018/8/12
+
+from __future__ import absolute_import
+
+from .wrapper import PVWeight
+from .attention import AttentionCell, MultiHeadAttentionCell, MLPAttentionCell, DotProductAttentionCell
