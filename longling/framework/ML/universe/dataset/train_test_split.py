429f09d986d54464e8c10500696feb9042df6547
tswsxk <tongsw@mail.ustc.edu.cn>
2018-10-09 17:11:58 +0800

    增加提示信息

--- longling/framework/ML/universe/dataset/train_test_split.py
+++ longling/framework/ML/universe/dataset/train_test_split.py
@@ -19,10 +19,16 @@ def train_test_split(filename, test_ratio=0.5, root_dir=None, train_filename=Non
         train_filename = os.path.join(root_dir, train_filename)
         test_filename = os.path.join(root_dir, test_filename)
 
+    train_cnt = 0
+    test_cnt = 0
     with open(filename) as f, wf_open(train_filename) as train_wf, wf_open(test_filename) as test_wf:
         for line in tqdm(f, disable=not progress_viz):
             if random.random() < test_ratio:
                 wf = test_wf
+                test_cnt += 1
             else:
                 wf = train_wf
+                train_cnt += 1
             print(line, end='', file=wf)
+
+    return train_cnt, test_cnt
