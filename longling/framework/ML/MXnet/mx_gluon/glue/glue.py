cc5974c7d561823f120dcb626ddd694d293eb6ed
tongshiwei <tongsw@mail.ustc.edu.cn>
2018-10-10 20:59:27 +0800

    NI

--- longling/framework/ML/MXnet/mx_gluon/glue/glue.py
+++ longling/framework/ML/MXnet/mx_gluon/glue/glue.py
@@ -54,7 +54,7 @@ def new_module(module_name, directory=None):
 if __name__ == '__main__':
     import argparse
 
-    module_name = "DKT"
+    module_name = "DQN"
     parser = argparse.ArgumentParser()
     if module_name:
         parser.add_argument("--module_name", default="%s" % module_name,
