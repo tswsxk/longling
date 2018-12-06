8f0f3d9208851805ae780845b15ea704fb238f7b
tswsxk <tongsw@mail.ustc.edu.cn>
2018-12-06 14:22:47 +0800

    更新版本

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin
 # python setup.py check && python setup.py sdist && python setup.py register sdist upload
-python3 setup.py sdist bdist_wheel && twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python3 setup.py sdist bdist_wheel && twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
