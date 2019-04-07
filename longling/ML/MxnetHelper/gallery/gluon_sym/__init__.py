5e87ba70f13ccb406cfb533466a810b9fb5589b1
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-07 16:03:21 +0800

    remove old network module

--- longling/ML/MxnetHelper/gallery/gluon_sym/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-# created by tongshiwei on 2018/7/11
-
-from __future__ import absolute_import
-
-from .nn_loss import *
-from longling.ML.MxnetHelper.gallery.nn_sym import *
-
-# todo TextCNN3D, ATN_RNN
