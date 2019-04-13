4bf73f9c9b902126c7ca1c72e9a59d7e4c9d86dc
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-13 09:57:26 +0800

    add demo case to show how to assign the embedding layer weight

---
+++ longling/ML/MxnetHelper/gallery/demo/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+# create by tongshiwei on 2019/4/13
+
+from .embedding import set_embedding_weight
---
+++ longling/ML/MxnetHelper/gallery/demo/embedding.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# create by tongshiwei on 2019/4/13
+
+from mxnet.gluon.nn import Embedding
+from mxnet.ndarray import NDArray
+
+
+def set_embedding_weight(embedding_layer, weight):
+    """
+    Parameters
+    ----------
+    embedding_layer: Embedding
+    weight: NDArray
+    """
+    embedding_layer.weight.set_data(weight)
