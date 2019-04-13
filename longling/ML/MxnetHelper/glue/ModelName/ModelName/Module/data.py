30c99b69cfda782551ba523ebf4a25223c756887
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-13 09:58:10 +0800

    add new script to implement the functions related to data process

---
+++ longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/data.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# create by tongshiwei on 2019/4/12
+__all__ = ["load_data", "transform", "get_data_iter"]
+
+from mxnet import gluon
+from tqdm import tqdm
+
+
+def load_data(data_src):
+    raise NotImplementedError
+
+
+def transform(raw_data, params):
+    # 定义数据转换接口
+    # raw_data --> batch_data
+
+    batch_size = params.batch_size
+
+    return gluon.data.DataLoader(gluon.data.ArrayDataset(raw_data), batch_size)
+
+
+def get_data_iter(*args, **kwargs):
+    raw_data = load_data(*args)
+    transform(raw_data, **kwargs)
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    for data in tqdm(get_data_iter()):
+        pass
