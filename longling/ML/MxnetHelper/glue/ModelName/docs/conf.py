c3116c1505c8a2b7e883c0b0eb21de7daaa6a4d7
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-15 08:40:48 +0800

    文档名修改

--- longling/ML/MxnetHelper/glue/ModelName/docs/conf.py
+++ longling/ML/MxnetHelper/glue/ModelName/docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'module_name'
+project = 'ModelName'
 copyright = '2018, tongshiwei'
 author = 'tongshiwei'
 
