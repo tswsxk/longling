76da868eede19797380d9303fa7126fdc89cb7ba
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-18 17:22:00 +0800

    add toolkit dependency

--- longling/ML/toolkit/__init__.py
+++ longling/ML/toolkit/__init__.py
@@ -1,2 +1,4 @@
 # coding: utf-8
 # create by tongshiwei on 2019/4/7
+
+from .dependency import __dependency__
---
+++ longling/ML/toolkit/dependency.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+# create by tongshiwei on 2019/4/18
+
+__dependency__ = ["tqdm", "matplotlib"]
