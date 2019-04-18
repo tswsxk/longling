ef7791a44402ab10b3d49ebed4ec4bb7601f793b
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-18 16:03:58 +0800

    add __all__

--- longling/ML/MxnetHelper/gallery/layer/__init__.py
+++ longling/ML/MxnetHelper/gallery/layer/__init__.py
@@ -4,3 +4,9 @@
 from .highway import HighwayCell
 from .normalization import get_l2_embedding_weight
 from .sequence import format_sequence, mask_sequence_variable_length
+
+__all__ = [
+    "HighwayCell",
+    "get_l2_embedding_weight",
+    "format_sequence", "mask_sequence_variable_length",
+]
