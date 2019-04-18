02024b1392e301d42de43fb6e4714b73b4044b0f
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-18 16:05:16 +0800

    add attention

--- longling/ML/MxnetHelper/gallery/layer/__init__.py
+++ longling/ML/MxnetHelper/gallery/layer/__init__.py
@@ -1,11 +1,15 @@
 # coding: utf-8
 # create by tongshiwei on 2019/4/7
 
+from .attention import AttentionCell, MultiHeadAttentionCell, \
+    MLPAttentionCell, DotProductAttentionCell
 from .highway import HighwayCell
 from .normalization import get_l2_embedding_weight
 from .sequence import format_sequence, mask_sequence_variable_length
 
 __all__ = [
+    "AttentionCell", "MultiHeadAttentionCell", "MLPAttentionCell",
+    "DotProductAttentionCell",
     "HighwayCell",
     "get_l2_embedding_weight",
     "format_sequence", "mask_sequence_variable_length",
