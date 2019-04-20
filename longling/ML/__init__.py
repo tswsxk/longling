1ba1d482e701e0f89ace50c3bce561befe751ff2
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-04-20 19:10:58 +0800

    remove dependency

--- longling/ML/__init__.py
+++ longling/ML/__init__.py
@@ -5,9 +5,3 @@ from __future__ import absolute_import
 
 from . import MxnetHelper
 from . import toolkit
-
-__dependency__ = list(set(
-    MxnetHelper.__dependency__ + toolkit.__dependency__
-))
-
-__all__ = ["__dependency__"]
