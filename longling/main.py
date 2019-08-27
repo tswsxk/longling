04d762b9b2e39c2c968d460646a78e5c4fd0c979
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-08-27 23:26:00 +0800

    新增命令行工具

---
+++ longling/main.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# create by tongshiwei on 2019-8-27
+
+import fire
+
+from longling.ML.toolkit.dataset import train_valid_test, train_test, train_valid, kfold
+
+if __name__ == '__main__':
+    fire.Fire()
