6a25bd5a588ba4d9095cdba776990a7393fbd66b
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-09-01 22:12:21 +0800

    导出控制

--- longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/configuration.py
+++ longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/configuration.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 # Copyright @tongshiwei
-
 from __future__ import absolute_import
 from __future__ import print_function
+
+__all__ = ["Configuration", "ConfigurationParser"]
 
 import datetime
 import pathlib
