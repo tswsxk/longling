01eacd25318326ce39c98d0183d25593631d65be
tswsxk <tongsw@mail.ustc.edu.cn>
2019-09-21 22:10:38 +0800

    新增变量替换正则表达式

--- longling/lib/process_pattern/__init__.py
+++ longling/lib/process_pattern/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import
 
 from .process_pattern import PatternHitter
 from .condition_filter import *
+from .variable import *
 
-__all__ = ["PatternHitter"]
+__all__ = ["PatternHitter", "variable_replace", "default_variable_replace"]
