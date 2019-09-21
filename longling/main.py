7143e98c486bfcf3b7efa70fd218876a4a65a7f0
tswsxk <tongsw@mail.ustc.edu.cn>
2019-09-21 22:17:56 +0800

    新增arch模块

--- longling/main.py
+++ longling/main.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 # create by tongshiwei on 2019-8-27
 
+import functools
 import fire
 
 from longling.ML.toolkit.dataset import train_valid_test, train_test, train_valid, kfold
 from longling.toolbox import toc
+from longling.Architecture import cli as arch
+from longling import Architecture as arch_cli
+
+
+def tarch():
+    return functools.partial(arch, author="Shiwei Tong")
 
 
 def cli():
@@ -15,6 +22,9 @@ def cli():
             "train_valid": train_valid,
             "kfold": kfold,
             "toc": toc,
+            "arch": arch,
+            "tarch": tarch,
+            "arch-cli": arch_cli,
         }
     )
 
