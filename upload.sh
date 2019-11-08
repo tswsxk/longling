5787b70e202c47433518c6613031d095db1da5d1
Sherlcok <tongsw@mail.ustc.edu.cn>
2019-11-08 11:10:03 +0800

    修正语法错误

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin
 # python setup.py.T check && python setup.py.T sdist && python setup.py.T register sdist upload
-python3 setup.py.T sdist bdist_wheel && twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+python3 setup.py sdist bdist_wheel && twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
