b6c28a75d4cf666e0925e3d572976dde84589688
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-10 15:16:57 +0800

    create new option in cli, which can be used to get more information of the result

--- longling/ML/toolkit/analyser/cli.py
+++ longling/ML/toolkit/analyser/cli.py
@@ -12,10 +12,27 @@ def key_parser(key):
     return key
 
 
-def _select_max(src, *keys):
+def get_by_key(data, parsed_key):
+    _key = key_parser(parsed_key)
+    _data = data
+    if isinstance(_key, list):
+        for k in _key:
+            _data = _data[k]
+    else:
+        _data = _data[_key]
+    return _data
+
+
+def _select_max(src, *keys, with_keys: (str, None) = None, with_all=False):
     keys = as_list(keys)
 
+    with_keys = [] if with_keys is None else with_keys.split(";")
+
     result = {
+        key: None for key in keys
+    }
+
+    result_appendix = {
         key: None for key in keys
     }
 
@@ -23,26 +40,31 @@ def _select_max(src, *keys):
         for line in f:
             data = json.loads(line)
             for key in result:
-                _key = key_parser(key)
-                _data = data
-                if isinstance(_key, list):
-                    for k in _key:
-                        _data = _data[k]
-                else:
-                    _data = _data[_key]
+                _data = get_by_key(data, parsed_key=key_parser(key))
                 if result[key] is None or _data > result[key]:
                     result[key] = _data
+                    if with_all:
+                        result_appendix[key] = data
+                    elif with_keys:
+                        result_appendix[key] = [(_key, get_by_key(data, key_parser(_key))) for _key in with_keys]
+
     for item in result.items():
-        print(item)
+        print(item, end='')
+        if result_appendix[item[0]]:
+            print(" ", result_appendix[item[0]])
+        else:
+            print()
 
 
-def arg_select_max(*keys, src):
-    _select_max(src, *keys)
+def arg_select_max(*keys, src, with_keys=None, with_all=False):
+    _select_max(src, *keys, with_keys=with_keys, with_all=with_all)
 
 
-def select_max(src, *keys):
-    _select_max(src, *keys)
+def select_max(src, *keys, with_keys=None, with_all=False):
+    _select_max(src, *keys, with_keys=with_keys, with_all=with_all)
 
 
 if __name__ == '__main__':
-    arg_select_max("auc", "prf_1_f1", src="result.json")
+    arg_select_max("auc", "prf:1:f1", src="result.json")
+    arg_select_max("auc", "prf:1:f1", src="result.json", with_all=True)
+    arg_select_max("auc", "prf:1:f1", src="result.json", with_keys="Epoch;train_time")
