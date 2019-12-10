97b435a2a0be32a1806243700798453eecd6d56b
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-10 10:08:26 +0800

    postphone test for ML

--- tests/test_ML/test_toolkit/test_dataset.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-# create by tongshiwei on 2019/6/27
-
-from tests.lib.stream import NullDevice
--- tests/test_ML/test_toolkit/test_formatter.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-# create by tongshiwei on 2019/6/27
-
-from longling.ML.toolkit.formatter import MultiClassEvalFormatter
