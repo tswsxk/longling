0bc399c8b7e014154434642c2a5fe9684c337a20
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-11 13:53:22 +0800

    test for lib.path

---
+++ tests/test_lib/test_path.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# 2019/12/11 @ tongshiwei
+
+from longling import path_append, wf_open
+from longling import file_exist, abs_current_dir
+
+
+def test_path(tmp_path):
+    assert path_append(tmp_path, "../data", "../dataset1/", "train", to_str=True) == '../../data/../dataset1/train'
+
+    tmp_file = path_append(tmp_path, "test_path.txt")
+    with wf_open(tmp_file) as wf:
+        print("hello world", file=wf)
+
+    assert file_exist(tmp_file)
+    abs_current_dir(tmp_file)
