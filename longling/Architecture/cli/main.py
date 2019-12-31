55fe981b8615c4497fd3d7ca1d50b598e3298c50
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-31 14:48:45 +0800

    coverage fix and add annotation

--- longling/Architecture/cli/main.py
+++ longling/Architecture/cli/main.py
@@ -9,7 +9,10 @@ from longling.Architecture.install_proj import project_types
 from longling.Architecture.cli.units import *
 
 
-def cli(skip_top=True, project=None, **kwargs):
+def cli(skip_top=True, project=None, **kwargs):  # pragma: no cover
+    """
+    The main function for arch
+    """
     main_params = main_cli(project, **kwargs)
 
     kwargs.update(main_params)
@@ -27,7 +30,3 @@ def cli(skip_top=True, project=None, **kwargs):
     __project_type = main_params["project_type"]
 
     project_types[__project_type](tar_dir=tar_dir, main_params=main_params, docs_params=docs_params)
-
-
-if __name__ == '__main__':
-    cli()
