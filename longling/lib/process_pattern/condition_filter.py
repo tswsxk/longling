3e8947104aff3a53aef7775c07f1dd237bd80276
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-31 11:06:15 +0800

    add examples

--- longling/lib/process_pattern/condition_filter.py
+++ longling/lib/process_pattern/condition_filter.py
@@ -2,7 +2,22 @@ import re
 
 
 class CommentFilter(object):
-    # 评论内容过滤器，过滤 只含表情，数字 以及 回复（含有 //@ ）评论
+    """
+    评论内容过滤器，过滤 只含表情，数字 以及 回复（含有 //@ ）评论
+
+    Examples
+    --------
+    >>> cf = CommentFilter()
+    >>> cf.is_valid("🐶😂")
+    False
+    >>> cf.is_valid("//@groot: hello world")
+    False
+    >>> cf.is_valid("233")
+    False
+    >>> cf.is_valid("再给我两分钟")
+    True
+    """
+
     def __init__(self):
         self.emoji_re = re.compile('['
                                    '\U0001F300-\U0001F64F'
@@ -10,8 +25,8 @@ class CommentFilter(object):
                                    '\u2600-\u2B55]+',
                                    re.UNICODE)
 
-    def is_valid(self, data):
-        if re.sub("^\d+$", "", self.emoji_re.sub("", data['text'])):
-            if "//@" not in data['text']:
+    def is_valid(self, string: str):
+        if re.sub("^\d+$", "", self.emoji_re.sub("", string)):
+            if "//@" not in string:
                 return True
         return False
