1184979ad0a95ea610378e0a19b00ff426b7222d
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-31 13:53:14 +0800

    switch the release method to Makefile

--- upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin
-# python setup.py.T check && python setup.py.T sdist && python setup.py.T register sdist upload
-python3 setup.py sdist bdist_wheel && twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
--- upload_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin
-python setup.py sdist bdist_wheel && twine check dist/* && twine upload --repository-url https://test.pypi.org/legacy/ dist/*
