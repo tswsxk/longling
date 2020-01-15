e86021a4ea4d6d55102823f578349f1f58a5f63d
tswsxk <tongsw@mail.ustc.edu.cn>
2020-01-15 18:24:34 +0800

    fix parallel bugs in pytorch

--- longling/ML/PytorchHelper/toolkit/parallel.py
+++ longling/ML/PytorchHelper/toolkit/parallel.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # create by tongshiwei on 2019-8-30
+import logging
 import torch
 from torch.nn import DataParallel
 
@@ -13,17 +14,22 @@ def set_device(_net, ctx, **kwargs):
                 torch.ones((1,), device=torch.device("cuda: 0"))
             except AssertionError as e:
                 raise TypeError("no cuda detected, noly cpu is supported, the detailed error msg:%s" % str(e))
-        if torch.cuda.device_count() > 1:
+        if torch.cuda.device_count() >= 1:
             if ":" in ctx:
                 ctx_name, device_ids = ctx.split(":")
                 assert ctx_name in ["cuda", "gpu"], "the equipment should be 'cpu', 'cuda' or 'gpu', now is %s" % ctx
                 device_ids = [int(i) for i in device_ids.strip().split(",")]
-                return DataParallel(_net, device_ids).cuda()
+                try:
+                    return DataParallel(_net, device_ids).cuda()
+                except AssertionError as e:
+                    logging.error(device_ids)
+                    raise e
             elif ctx in ["cuda", "gpu"]:
                 return DataParallel(_net).cuda()
             else:
                 raise TypeError("the equipment should be 'cpu', 'cuda' or 'gpu', now is %s" % ctx)
         else:
+            print(torch.cuda.device_count())
             raise TypeError("0 gpu can be used, use cpu")
     else:
         return DataParallel(_net, device_ids=ctx).cuda()
