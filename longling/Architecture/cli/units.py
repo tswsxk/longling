931f52b6e9da53a1ec04012d59543b7109f70f04
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-03-03 19:22:34 +0800

    update

--- longling/Architecture/cli/units.py
+++ longling/Architecture/cli/units.py
@@ -3,12 +3,20 @@
 
 __all__ = ["main_cli", "docs_cli"]
 
+import os
+import pathlib
 import datetime
 from longling.Architecture.install_proj import project_types
-from longling.Architecture.cli.utils import legal_input
+from longling.Architecture.cli.utils import legal_input, binary_legal_input
 
 
-def main_cli(project, **kwargs):  # pragma: no cover
+def main_cli(skip_top, project, **kwargs):  # pragma: no cover
+    if skip_top:
+        default_project_name = pathlib.PurePath(os.getcwd()).name
+        project = legal_input("Project Name (default is %s) < " % default_project_name,
+                              __default_value="%s" % default_project_name) if not project else project
+    else:
+        project = legal_input("Project Name < ", is_legal=lambda x: True if x else False) if not project else project
     params = dict(
         project=legal_input("Project Name < ", is_legal=lambda x: True if x else False) if not project else project,
         project_type=legal_input("Project Type (%s) < " % "/".join(project_types), __legal_input=project_types),
@@ -16,25 +24,29 @@ def main_cli(project, **kwargs):  # pragma: no cover
     return params
 
 
-def docs_cli(project=None, title=None, author=None, copyright=None, **kwargs):  # pragma: no cover
+def docs_cli(project=None, title=None, author=None, copyright=None, default_style="sphinx",
+             **kwargs):  # pragma: no cover
     params = dict(
         project=legal_input("Project Name < ", is_legal=lambda x: True if x else False) if not project else project,
-        docs_root="docs/" if legal_input("mkdir docs? (y/n, default is n) < ",
-                                         __legal_input={"y", "n"}, __default_value='n') == "y" else "./"
+        docs_style=legal_input("Docs Style (mxnet/sphinx, default is %s)? < " % default_style,
+                               __legal_input={"mxnet", "sphinx"},
+                               __default_value="%s" % default_style),
+        docs_root="docs/" if binary_legal_input("Make 'docs/' directory?", _default="y") else "./",
     )
-    params.update(dict(
-        title=legal_input("Docs Title (default is %s) < " % params["project"],
-                          __default_value=params["project"]) if not title else title,
-        author=legal_input("Author < ", is_legal=lambda x: True if x else False) if not author else author,
+    if params["docs_style"] != "sphinx":
+        params.update(dict(
+            title=legal_input("Docs Title (default is %s) < " % params["project"],
+                              __default_value=params["project"]) if not title else title,
+            author=legal_input("Author < ", is_legal=lambda x: True if x else False) if not author else author,
 
-    ))
+        ))
 
-    default_copyright = "%s, %s" % (datetime.date.today().year, params["author"])
-    params.update(dict(
-        copyright=legal_input("Copyright (default is %s) < " % default_copyright,
-                              __default_value=default_copyright) if not copyright else copyright,
-        readthedocs=True if legal_input("Install .readthedocs.yml (y/n, default is y) < ", __legal_input={'y', 'n'},
-                                        __default_value='y') == 'y' else False
-    ))
+        default_copyright = "%s, %s" % (datetime.date.today().year, params["author"])
+        params.update(dict(
+            copyright=legal_input("Copyright (default is %s) < " % default_copyright,
+                                  __default_value=default_copyright) if not copyright else copyright,
+        ))
+    params.update(readthedocs=binary_legal_input("Install .readthedocs.yml?"))
+    params.update(dockerfile=binary_legal_input("Install Dockerfile for documents?"))
 
     return params
