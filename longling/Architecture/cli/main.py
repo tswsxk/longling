00e7564f59b8d5b81feefc55c49e37c073ad3fb3
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-03-03 19:37:56 +0800

    add k8s

--- longling/Architecture/cli/main.py
+++ longling/Architecture/cli/main.py
@@ -3,8 +3,8 @@
 
 __all__ = ["cli"]
 
-from longling.lib.stream import build_dir
-from longling.Architecture.cli.utils import legal_input
+import os
+from longling.Architecture.cli.utils import legal_input, binary_legal_input
 from longling.Architecture.install_proj import project_types
 from longling.Architecture.cli.units import *
 
@@ -13,24 +13,34 @@ def cli(skip_top=True, project=None, **kwargs):  # pragma: no cover
     """
     The main function for arch
     """
-    main_params = main_cli(project, **kwargs)
+    main_params = main_cli(skip_top, project, **kwargs)
 
     kwargs.update(main_params)
-    if main_params["project_type"] == "docs" or legal_input("Install docs (y/n, default is y) < ",
-                                                            __legal_input={'y', 'n'}, __default_value='y') == 'y':
-        docs_params = docs_cli(**kwargs)
+
+    indicator = dict(
+        traivs=binary_legal_input("Install travis?"),
+    )
+
+    if main_params["project_type"] == "docs" or binary_legal_input("Install docs?"):
+        default_style = "mxnet" if main_params["project_type"] == "docs" else "sphinx"
+        docs_params = docs_cli(default_style=default_style, **kwargs)
     else:
         docs_params = {}
 
-    indicator = dict(
-        travis=True if legal_input("Install docs (y/n, default is y) < ", __legal_input={'y', 'n'},
-                                   __default_value='y') == 'y' else False
-    )
+    if main_params["project_type"] != "docs":
+        if binary_legal_input("To deploy as a service"):
+            indicator.update(dict(
+                service_type=legal_input(
+                    "Choose a service type (cli/flask/nginx) < ",
+                    __legal_input={"cli", "flask", "nginx"},
+                )
+            ))
 
     if skip_top:
         tar_dir = "./"
     else:
-        tar_dir = build_dir("%s" % main_params["project"])
+        tar_dir = main_params["project"]
+        os.makedirs(tar_dir)
 
     __project_type = main_params["project_type"]
 
