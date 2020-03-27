3b417d609c757168e0cfc86e386cb02b7923be7c
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-03-27 09:18:33 +0800

    fix error

--- tests/test_spider/test_download.py
+++ tests/test_spider/test_download.py
@@ -5,16 +5,13 @@ import pytest
 from longling import path_append
 from longling.spider.download_data import download_file
 from longling.spider.utils import reporthook4urlretrieve
-from longling.lib.path import file_exist
 
 
 def test_download_data(tmp_path):
-    url = "http://base.ustc.edu.cn/data/EdNet/EdNet-Contents.zip "
+    url = "http://base.ustc.edu.cn/data/EdNet/EdNet-Contents.zip"
     tmp_file = path_append(tmp_path, "download", to_str=True)
 
-    download_file(url, decomp=False)
-
-    download_file(url)
+    download_file(url, save_path=tmp_file, decomp=False)
 
     download_file(url, tmp_file)
     download_file(url, tmp_file, reporthook=reporthook4urlretrieve)
