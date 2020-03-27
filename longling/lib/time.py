aab55f5f6a79c983110565f62aee85256a712485
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-03-27 17:26:47 +0800

    add example

--- longling/lib/time.py
+++ longling/lib/time.py
@@ -4,5 +4,14 @@
 import datetime
 
 
-def get_current_timestamp():
+def get_current_timestamp() -> str:  # pragma: no cover
+    """
+    Examples
+    ---------
+    .. code-block :: python
+
+    get_current_timestamp()
+    '20200327172235'
+
+    """
     return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
