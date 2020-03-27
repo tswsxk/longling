820b98ee0a5594ec2f95af0dafa19de2e7164fcb
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-03-27 14:44:59 +0800

    removing patterner test

--- tests/test_lib/test_process_pattern.py
+++ tests/test_lib/test_process_pattern.py
@@ -3,9 +3,12 @@
 
 import pytest
 
-from longling.lib.process_pattern import variable_replace, PatternHitter
-from longling.lib.process_pattern.pattener import mode_dict, register
-from longling import path_append, wf_open, rf_open
+from longling.lib.regex import variable_replace
+
+
+# from longling.lib.regex import PatternHitter
+# from longling.lib.regex.pattener import mode_dict, register
+# from longling import path_append, wf_open, rf_open
 
 
 def test_variable_replace():
@@ -21,30 +24,30 @@ def test_variable_replace():
 
     assert variable_replace("$PROJECT-", project="longling") == "longling-"
 
-
-def test_patterner():
-    tmp = mode_dict[0]
-
-    @register(0)
-    def _init_patterns0(line, pps=None):
-        return mode_dict[0](line, pps)
-
-    mode_dict[0] = tmp
-
-
-def test_pattern_hitter(tmpdir):
-    regex_txt = path_append(tmpdir, "process_pattern_regex.txt", to_str=True)
-    regex_string = ":世界\n:和平"
-    with wf_open(regex_txt) as wf:
-        print(regex_string, file=wf)
-    p = PatternHitter(regex_txt, mode=1)
-    assert p.hit_num("世界 和平") == 2
-
-    assert p.hit_group("世界 和平") == [":世界", ":和平"]
-
-    for mode in mode_dict:
-        with pytest.raises(ValueError):
-            PatternHitter("1:2:3", mode=mode)
-
-    with pytest.raises(KeyError):
-        PatternHitter("1:2", mode=2)
+#
+# def test_patterner():
+#     tmp = mode_dict[0]
+#
+#     @register(0)
+#     def _init_patterns0(line, pps=None):
+#         return mode_dict[0](line, pps)
+#
+#     mode_dict[0] = tmp
+#
+#
+# def test_pattern_hitter(tmpdir):
+#     regex_txt = path_append(tmpdir, "process_pattern_regex.txt", to_str=True)
+#     regex_string = ":世界\n:和平"
+#     with wf_open(regex_txt) as wf:
+#         print(regex_string, file=wf)
+#     p = PatternHitter(regex_txt, mode=1)
+#     assert p.hit_num("世界 和平") == 2
+#
+#     assert p.hit_group("世界 和平") == [":世界", ":和平"]
+#
+#     for mode in mode_dict:
+#         with pytest.raises(ValueError):
+#             PatternHitter("1:2:3", mode=mode)
+#
+#     with pytest.raises(KeyError):
+#         PatternHitter("1:2", mode=2)
