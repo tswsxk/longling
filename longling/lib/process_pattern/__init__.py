95bad2f60c9bf35f9127a176d1f1071d9505c056
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-03-27 14:39:08 +0800

    move regex lib from lib to nlp

--- longling/lib/process_pattern/__init__.py
+++ longling/lib/process_pattern/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-from .process_pattern import PatternHitter
-from .condition_filter import *
 from .variable import *
 
-__all__ = ["PatternHitter", "variable_replace", "default_variable_replace"]
+__all__ = ["variable_replace", "default_variable_replace"]
