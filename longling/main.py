4fcf33ec0bbe418e6718f5024f76aaa19f3a7e54
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-14 23:15:54 +0800

    new cli: min and amin

--- longling/main.py
+++ longling/main.py
@@ -6,7 +6,7 @@ import fire
 
 from longling.lib.stream import encode
 from longling.ML.toolkit.dataset import train_valid_test, train_test, train_valid, kfold
-from longling.ML.toolkit.analyser.cli import select_max, arg_select_max
+from longling.ML.toolkit.analyser.cli import select_max, arg_select_max, select_min, arg_select_min
 from longling.ML.toolkit.hyper_search.nni import show_top_k, show
 from longling.toolbox import toc
 from longling.Architecture.cli import cli as arch
@@ -31,6 +31,8 @@ def cli():  # pragma: no cover
             "arch-cli": arch_cli,
             "max": select_max,
             "amax": arg_select_max,
+            "min": select_min,
+            "amin": arg_select_min,
             "csv2jsonl": csv2jsonl,
             "jsonl2csv": jsonl2csv,
             "encode": encode,
