a5041582ea46d5682af886d25bdf5da115169e3e
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-14 15:19:33 +0800

    deprecated

--- longling/ML/toolkit/analyser/analyser.py
+++ longling/ML/toolkit/analyser/analyser.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 __all__ = ["ResultAnalyser"]
 
 warnings.warn("do not use this package, unstable")
+warnings.warn("do not use this package, deprecated")
 
 
 class ResultAnalyser(object):
