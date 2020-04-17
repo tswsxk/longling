fef1d19427cce7a0ffcd454a6d03ea4a0f6cd2bb
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-17 17:13:54 +0800

    switch to new implementation of train_test, train_valid_test and kfold

--- longling/ML/toolkit/dataset/__init__.py
+++ longling/ML/toolkit/dataset/__init__.py
@@ -2,7 +2,9 @@
 # created by tongshiwei on 2018/10/9
 
 
-from .indicesor import Indicesor
-from .splitter.file_splitter import *
-from .splitter.iterable_splitter import *
-from .splitter.splitter import Splitter
+# from .indicesor import Indicesor
+# from .splitter.file_splitter import *
+# from .splitter.iterable_splitter import *
+# from .splitter.splitter import Splitter
+
+from .split import train_test, train_valid_test, kfold
