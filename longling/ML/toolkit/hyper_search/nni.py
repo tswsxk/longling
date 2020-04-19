a8436c08cfa04a88fcc16d34424b03655e0066cb
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-19 14:00:02 +0800

    add docs

--- longling/ML/toolkit/hyper_search/nni.py
+++ longling/ML/toolkit/hyper_search/nni.py
@@ -28,7 +28,8 @@ def _key(x):
         return float(json.loads(x)["default"])
 
 
-def show(key, max_key=True, exp_id=None, res_dir="./", nni_dir=path_append(os.environ.get("HOME", "./"), "nni/experiments"),
+def show(key, max_key=True, exp_id=None, res_dir="./",
+         nni_dir=path_append(os.environ.get("HOME", "./"), "nni/experiments"),
          only_final=False,
          with_keys=None, with_all=False):  # pragma: no cover
     """
@@ -66,9 +67,13 @@ def show(key, max_key=True, exp_id=None, res_dir="./", nni_dir=path_append(os.en
             trial_params = json.load(f)["parameters"]
         trial_res = path_append(res_dir, trial, "result.json", to_str=True)
         if max_key:
-            value, appendix = get_max(trial_res, key, with_keys=with_keys, with_all=with_all)
+            value, appendix = get_max(
+                trial_res, key, with_keys=with_keys, with_all=with_all, merge=False
+            )
         else:
-            value, appendix = get_min(trial_res, key, with_keys=with_keys, with_all=with_all)
+            value, appendix = get_min(
+                trial_res, key, with_keys=with_keys, with_all=with_all, merge=False
+            )
         if with_keys is not None or with_all is True:
             result.append([trial, trial_params, value, dict(appendix[key])])
         else:
@@ -120,9 +125,33 @@ class BaseReporter(object):
         raise NotImplementedError
 
 
-def get_params(received_params: dict, cfg_cls: Configuration):
+def get_params(received_params: dict, cfg_cls: (Configuration, type(Configuration))):
+    """
+    Parameters
+    ----------
+    received_params: dict
+        nni get_next_parameters() 得到的参数字典
+    cfg_cls: Configuration
+        配置文件实例
+
+    Returns
+    -------
+    cfg_params: dict
+        存在与 cfg 中 received_params 的更新值
+    unk_params: dict
+        不存在与 cfg 中 received_params 的更新值
+
+    Examples
+    --------
+    >>> class CFG(Configuration):
+    ...     hyper_params = {"hidden_num": 100}
+    ...     learning_rate = 0.001
+    >>> cfg = CFG()
+    >>> get_params({"hidden_num": 50, "learning_rate": 0.1, "act": "relu"}, cfg)
+    ({'hyper_params': {'hidden_num': 50}, 'learning_rate': 0.1}, {'act': 'relu'})
+    """
     cfg_params = {}
-    u_params = {}
+    unk_params = {}
 
     path, _ = dict2pv(cfg_cls.vars())
 
@@ -135,13 +164,100 @@ def get_params(received_params: dict, cfg_cls: Configuration):
             if k in keys:
                 cfg_params.update(list2dict(keys[k], v))
             else:
-                u_params[k] = v
+                unk_params[k] = v
 
-    return cfg_params, u_params
+    return cfg_params, unk_params
 
 
-def prepare_hyper_search(cfg_kwargs: dict, cfg_cls, reporthook=None, final_reporthook=None,
-                         primary_key=None, max_key=True, reporter_cls=None, with_keys: (list, str, None) = None, dump=False):
+def prepare_hyper_search(cfg_kwargs: dict, cfg_cls: (Configuration, type(Configuration)),
+                         reporthook=None, final_reporthook=None,
+                         primary_key=None, max_key=True, reporter_cls=None, with_keys: (list, str, None) = None,
+                         dump=False):
+    """
+    从 nni package 中获取超参，更新配置文件参数。当 nni 不可用或不是 nni 搜索模式时，参数将不会改变。
+
+    ..code-block :: python
+
+        cfg_kwargs, reporthook, final_reporthook, tag = prepare_hyper_search(
+            cfg_kwargs, Configuration, reporthook, final_reporthook, primary_key="macro_avg:f1"
+        )
+
+        _cfg = Configuration(**cfg_kwargs)
+        model = Model(_cfg)
+        ...
+
+        for epoch in range(_cfg.begin_epoch, _cfg.end_epoch):
+            for batch_data in dataset:
+                train_model(batch_data)
+
+            data = evaluate_model()
+            reporthook(data)
+
+        final_reporthook()
+
+    Parameters
+    ----------
+    cfg_kwargs: dict
+        待传入cfg的参数
+    cfg_cls: type(Configuration) or Configuration
+        配置文件
+    reporthook
+    final_reporthook
+    primary_key:
+        评估模型用的主键,
+        ``nni.report_intermediate_result`` 和 ``nni.report_final_result``中 ``metric`` 的 ``default``
+    max_key
+    reporter_cls
+    with_keys: list or str
+        其它要存储的 metric
+    dump: bool
+        为 True 时，会修改 配置文件 中 workspace 参数为 ``workspace/nni.get_experiment_id()/nni.get_trial_id()``
+        使得 nni 的中间结果会被存储下来。
+
+    Returns
+    -------
+    cfg_kwargs: dict
+        插入了nni超参后的配置文件参数
+    reporthook: function
+        每个iteration结束后的回调函数，用来报告中间结果。
+        默认 ``nni.report_intermediate_result``。
+    final_reporthook:
+        所有iteration结束后的回调函数，用来报告最终结果。
+        默认 ``nni.report_final_result``
+    dump: bool
+        和传入参数保持一致
+
+    Examples
+    --------
+    .. code-block :: python
+
+        class CFG(Configuration):
+            hyper_params = {"hidden_num": 100}
+            learning_rate = 0.001
+            workspace = ""
+
+        cfg_kwargs, reporthook, final_reporthook, dump = prepare_hyper_search(
+            {"learning_rate": 0.1}, CFG, primary_key="macro_avg:f1", with_keys="accuracy"
+        )
+        # cfg_kwargs: {'learning_rate': 0.1}
+
+    when nni start (e.g., using ``nni create --config _config.yml``),
+    suppose in ``_config.yml``:
+
+    .. code-block: yml
+
+        searchSpacePath: _search_space.json
+
+    and in ``_search_space.json``
+
+    .. code-block :: json
+
+        {
+            "hidden_num": {"_type": "choice", "_value": [500, 600, 700, 835, 900]},
+        }
+
+    one of the return cfg_kwargs is ``{'hyper_params': {'hidden_num': 50}, 'learning_rate': 0.1}``
+    """
     try:
         import nni
         from nni import get_next_parameter, report_intermediate_result, report_final_result
@@ -161,7 +277,10 @@ def prepare_hyper_search(cfg_kwargs: dict, cfg_cls, reporthook=None, final_repor
                 self.datas = []
 
             def intermediate(self, data):
-                feed_dict = {'default': float(data[primary_key])}
+                feed_dict = {
+                    'default': float(get_by_key(data, key_parser(primary_key))),
+                    primary_key: get_by_key(data, key_parser(primary_key))
+                }
                 for key in with_keys:
                     feed_dict[key] = get_by_key(data, key_parser(key))
                 report_intermediate_result(feed_dict)
@@ -169,12 +288,15 @@ def prepare_hyper_search(cfg_kwargs: dict, cfg_cls, reporthook=None, final_repor
 
             def final(self):
                 best_fn = get_min if max_key is False else get_max
-                final_result = best_fn(self.datas, primary_key, with_keys=";".join(with_keys) if with_keys else None)
+                _with_keys = (with_keys if with_keys else []) + [primary_key]
+                final_result = best_fn(
+                    self.datas, primary_key, with_keys=";".join(_with_keys), merge=False
+                )
                 feed_dict = {
                     'default': float(final_result[0][primary_key])
                 }
                 appendix_dict = dict(final_result[1][primary_key])
-                for key in with_keys:
+                for key in _with_keys:
                     feed_dict[key] = appendix_dict[key]
                 report_final_result(feed_dict)
 
@@ -184,13 +306,14 @@ def prepare_hyper_search(cfg_kwargs: dict, cfg_cls, reporthook=None, final_repor
         cfg_cls_params, hyper_params = get_params(get_next_parameter(), cfg_cls)
         using_nni_tag = True if cfg_cls_params or hyper_params else False
         cfg_kwargs.update(cfg_cls_params)
-        cfg_kwargs["hyper_params"].update(hyper_params)
-        if using_nni_tag is True and dump is True:
+        if "hyper_params" in cfg_kwargs:  # pragma: no cover
+            cfg_kwargs["hyper_params"].update(hyper_params)
+        if using_nni_tag is True and dump is True:  # pragma: no cover
             cfg_kwargs["workspace"] = cfg_kwargs.get("workspace", "") + path_append(
                 nni.get_experiment_id(), nni.get_trial_id(), to_str=True
             )
         return cfg_kwargs, reporthook, final_reporthook, dump
 
-    except ModuleNotFoundError:
+    except ModuleNotFoundError:  # pragma: no cover
         warnings.warn("nni package not found, skip")
         return cfg_kwargs, reporthook, final_reporthook, dump
