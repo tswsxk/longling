b12b8ca10c4aedee772ad9c11a080149112f9d88
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-19 10:39:38 +0800

    support find min key

--- longling/ML/toolkit/hyper_search/nni.py
+++ longling/ML/toolkit/hyper_search/nni.py
@@ -4,7 +4,7 @@
 import pathlib
 import warnings
 from heapq import nlargest
-from longling.ML.toolkit.analyser import get_max, get_by_key, key_parser
+from longling.ML.toolkit.analyser import get_max, get_min, get_by_key, key_parser
 from longling import Configuration, path_append
 from longling import dict2pv, list2dict
 
@@ -28,7 +28,7 @@ def _key(x):
         return float(json.loads(x)["default"])
 
 
-def show(key, exp_id=None, res_dir="./", nni_dir=path_append(os.environ.get("HOME", "./"), "nni/experiments"),
+def show(key, max_key=True, exp_id=None, res_dir="./", nni_dir=path_append(os.environ.get("HOME", "./"), "nni/experiments"),
          only_final=False,
          with_keys=None, with_all=False):  # pragma: no cover
     """
@@ -65,7 +65,10 @@ def show(key, exp_id=None, res_dir="./", nni_dir=path_append(os.environ.get("HOM
         with open(path_append(trial_dir, trial, "parameter.cfg")) as f:
             trial_params = json.load(f)["parameters"]
         trial_res = path_append(res_dir, trial, "result.json", to_str=True)
-        value, appendix = get_max(trial_res, key, with_keys=with_keys, with_all=with_all)
+        if max_key:
+            value, appendix = get_max(trial_res, key, with_keys=with_keys, with_all=with_all)
+        else:
+            value, appendix = get_min(trial_res, key, with_keys=with_keys, with_all=with_all)
         if with_keys is not None or with_all is True:
             result.append([trial, trial_params, value, dict(appendix[key])])
         else:
@@ -138,7 +141,7 @@ def get_params(received_params: dict, cfg_cls: Configuration):
 
 
 def prepare_hyper_search(cfg_kwargs: dict, cfg_cls, reporthook=None, final_reporthook=None,
-                         primary_key=None, reporter_cls=None, with_keys: (list, str, None) = None, dump=False):
+                         primary_key=None, max_key=True, reporter_cls=None, with_keys: (list, str, None) = None, dump=False):
     try:
         import nni
         from nni import get_next_parameter, report_intermediate_result, report_final_result
@@ -165,7 +168,8 @@ def prepare_hyper_search(cfg_kwargs: dict, cfg_cls, reporthook=None, final_repor
                 self.datas.append(data)
 
             def final(self):
-                final_result = get_max(self.datas, primary_key, with_keys=";".join(with_keys) if with_keys else None)
+                best_fn = get_min if max_key is False else get_max
+                final_result = best_fn(self.datas, primary_key, with_keys=";".join(with_keys) if with_keys else None)
                 feed_dict = {
                     'default': float(final_result[0][primary_key])
                 }
