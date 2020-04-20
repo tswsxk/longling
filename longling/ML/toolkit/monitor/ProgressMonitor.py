5269577f63d4c5703f8ba84feafc7f6ee433483f
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-20 08:39:09 +0800

    leave out ZeroDivision exception

--- longling/ML/toolkit/monitor/ProgressMonitor.py
+++ longling/ML/toolkit/monitor/ProgressMonitor.py
@@ -70,7 +70,7 @@ class ConsoleProgressMonitorPlayer(object):
     @property
     def progress(self):
         elapsed = self.timer.time()
-        if not elapsed:
+        if not elapsed:  # pragma: no cover
             return ""
         rate = self.n / elapsed
         elapsed = self.format_interval(int(elapsed))
