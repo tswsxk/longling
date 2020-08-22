8747797b279ee080f483b20991f0ba9aeee79e8d
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-08-22 09:09:15 +0800

    feat: update lr params handling

--- longling/ML/MxnetHelper/glue/module.py
+++ longling/ML/MxnetHelper/glue/module.py
@@ -60,9 +60,12 @@ class Module(DL.Module):
     @staticmethod
     def get_trainer(net, optimizer='sgd', optimizer_params=None, lr_params=None, select=None, logger=logging):
         """把优化器安装到网络上"""
-        if lr_params is not None:
-            from longling.ML.MxnetHelper.toolkit import get_lr_scheduler
-            optimizer_params["lr_scheduler"] = get_lr_scheduler(logger=logger, **lr_params)
+        if lr_params:
+            if "update_params" in lr_params and len(lr_params) == 1:
+                pass
+            else:
+                from longling.ML.MxnetHelper.toolkit import get_lr_scheduler
+                optimizer_params["lr_scheduler"] = get_lr_scheduler(logger=logger, **lr_params)
 
         trainer = gluon.Trainer(
             net.collect_params(select), optimizer, optimizer_params
