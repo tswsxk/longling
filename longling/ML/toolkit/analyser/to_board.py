1613021ed27f1391335bef4bf61c9398be242856
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-08-26 15:46:13 +0800

    fix: wrong tag

--- longling/ML/toolkit/analyser/to_board.py
+++ longling/ML/toolkit/analyser/to_board.py
@@ -13,5 +13,5 @@ def to_board(src, board_dir, global_step_field, *scalar_fields):
     with SummaryWriter(board_dir) as sw:
         for line in loading(src):
             for scalar_field in scalar_fields:
-                sw.add_scalar(tag="scalar_field", scalar_value=get_by_key(line, scalar_field),
+                sw.add_scalar(tag=scalar_field, scalar_value=get_by_key(line, scalar_field),
                               global_step=int(get_by_key(line, global_step_field)))
