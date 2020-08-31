2638575fba017744a47f0dbcd4833b94b53493a8
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-08-31 10:24:57 +0800

    fix: update nni dependency to be incompatible with sklearn requirement

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ spider_deps = [
 
 dl_deps = ml_mx_deps + ml_pytorch_deps
 
-ml_full_deps = ml_base_deps + dl_deps + ["nni>=1.7.1"]
+ml_full_deps = ml_base_deps + dl_deps + ["nni>=1.8"]
 
 full_deps = ml_full_deps + spider_deps
 
