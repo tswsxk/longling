3b384b02380aaf8b95da023381b345475bf0cabd
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-10-11 02:24:15 +0800

    [test] coverage

---
+++ tests/test_ML/test_utils.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+# create by tongshiwei on 2020-10-11
+
+import pytest
+
+from longling.ML import choice
+
+
+def test_choice():
+    obj = [0.1, 0.4]
+    with pytest.raises(ValueError):
+        choice(obj)
+
+    obj = {1: 0.1, 2: 0.4}
+    with pytest.raises(ValueError):
+        choice(obj)
+
+    with pytest.raises(TypeError):
+        choice(1)
