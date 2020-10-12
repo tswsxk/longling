72a310269d719eaae097b5fd3230f1a2d011d407
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-10-12 11:41:52 +0800

    [feat] export quick_glance

--- longling/DM/__init__.py
+++ longling/DM/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 # create by tongshiwei on 2019/5/25
 
-from .pdh import plot_numeric, auto_fti
+from .pdh import plot_numeric, auto_fti, quick_glance
 from .preprocessing import RankGaussianNormalizer
