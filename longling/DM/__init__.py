f3585046ffcc59ea539720e16c790bc701b6222e
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-10-13 11:33:42 +0800

    [feat] export rank_gaussian_normalize

--- longling/DM/__init__.py
+++ longling/DM/__init__.py
@@ -2,4 +2,4 @@
 # create by tongshiwei on 2019/5/25
 
 from .pdh import plot_numeric, auto_fti, quick_glance
-from .preprocessing import RankGaussianNormalizer
+from .preprocessing import RankGaussianNormalizer, rank_gaussian_normalize
