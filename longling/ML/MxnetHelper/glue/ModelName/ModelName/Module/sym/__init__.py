ed93fd54c05dfa8bd1b5c4046d3e374afd95144d
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-09 21:17:31 +0800

    [style]

--- longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/sym/__init__.py
+++ longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/sym/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # create by tongshiwei on 2019-9-1
 
-from .net import get_net, get_bp_loss
 from .fit_eval import fit_f, eval_f
-from .viz import net_viz
 from .init import net_init
+from .net import get_net, get_bp_loss
+from .viz import net_viz
