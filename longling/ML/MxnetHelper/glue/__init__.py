fd767e1d21064e713a7a3808e1b36cbeee08e218
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-11 22:05:09 +0800

    [feat] export MetaModule and MetaCfg

--- longling/ML/MxnetHelper/glue/__init__.py
+++ longling/ML/MxnetHelper/glue/__init__.py
@@ -3,5 +3,7 @@
 
 from __future__ import absolute_import
 
+from .ModelName import ModelName as MetaModel
+from .ModelName.ModelName.Module import Module as MetaModule, Configuration as MetaCfg
 from .glue import cli
 from .module import module_wrapper
