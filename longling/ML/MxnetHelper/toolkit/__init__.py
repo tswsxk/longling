c52476ab7c0786f171260ceb9d85e796f0fecbee
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-12 15:52:50 +0800

    [feat] add fit wrapper

--- longling/ML/MxnetHelper/toolkit/__init__.py
+++ longling/ML/MxnetHelper/toolkit/__init__.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 # create by tongshiwei on 2019/4/7
 
+from .ctx import split_and_load
 from .init import load_net, net_initialize
 from .lr_scheduler import get_lr_scheduler
 from .optimizer_cfg import get_optimizer_cfg
 from .persistence import save_params
+from .fit import fit_wrapper
 from .trainer import get_trainer
