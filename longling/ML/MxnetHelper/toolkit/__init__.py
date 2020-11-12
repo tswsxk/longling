8a93acefc3ee1f8cdefe2fa75528ab5eb1f66182
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-12 15:23:42 +0800

    [feat] add get_trainer function

--- longling/ML/MxnetHelper/toolkit/__init__.py
+++ longling/ML/MxnetHelper/toolkit/__init__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # create by tongshiwei on 2019/4/7
 
-from .optimizer_cfg import get_optimizer_cfg
 from .lr_scheduler import get_lr_scheduler
+from .optimizer_cfg import get_optimizer_cfg
+from .trainer import get_trainer
