33b9f74340a398d06f8cbbfbcaa7d253569b39e5
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-12 15:24:44 +0800

    [feat] add new exports

--- longling/ML/MxnetHelper/toolkit/__init__.py
+++ longling/ML/MxnetHelper/toolkit/__init__.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # create by tongshiwei on 2019/4/7
 
+from .init import load_net, net_initialize
 from .lr_scheduler import get_lr_scheduler
 from .optimizer_cfg import get_optimizer_cfg
 from .trainer import get_trainer
