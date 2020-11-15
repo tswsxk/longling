840fe985212809e695a5e2f37c6df4d61a9ae2d3
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-15 16:42:23 +0800

    [feat] add export

--- longling/ML/MxnetHelper/glue/ModelName/__init__.py
+++ longling/ML/MxnetHelper/glue/ModelName/__init__.py
@@ -4,3 +4,6 @@
 from __future__ import absolute_import
 
 from .ModelName import ModelName
+from .ModelName.Module import Module, net_init, Configuration, fit_f
+
+__all__ = ["ModelName", "Module", "net_init", "Configuration", "fit_f"]
