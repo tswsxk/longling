3c86dc8ec75d8aa2bab28ac3c215614b4544a2f4
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-15 16:49:26 +0800

    [feat] add flatten_df

--- longling/DM/pdh/op.py
+++ longling/DM/pdh/op.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 # create by tongshiwei on 2020-10-29
 
-__all__ = ["group_sort"]
+__all__ = ["group_sort", "flatten_df"]
+
+import itertools
 
 import pandas as pd
 
@@ -44,3 +46,20 @@ def group_sort(df: pd.DataFrame, group_by, sort_by,
         return df.reset_index(drop=True)
     else:  # pragma: no cover
         return df
+
+
+def flatten_df(df: pd.DataFrame):
+    """
+    Examples
+    -------
+    >>> import pandas as pd
+    >>> _df = pd.DataFrame({"a": [1, 2, 3], "b": [0, 1, 2]})
+    >>> flatten_df(_df)
+       a_0  b_0  a_1  b_1  a_2  b_2
+    0    1    0    2    1    3    2
+    """
+    num = len(df)
+    columns = df.columns
+    new_columns = list(itertools.chain(*[[column + "_%s" % i for column in columns] for i in range(num)]))
+    flatten_value = df.values.flatten().tolist()
+    return pd.DataFrame([flatten_value], columns=new_columns)
