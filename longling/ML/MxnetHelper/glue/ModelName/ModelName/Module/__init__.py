411d72b8481a86345e468f50aed8dfa4ea5ed996
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-15 16:44:40 +0800

    [refactor] add export

--- longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/__init__.py
+++ longling/ML/MxnetHelper/glue/ModelName/ModelName/Module/__init__.py
@@ -5,11 +5,11 @@ from __future__ import absolute_import
 from .configuration import Configuration, ConfigurationParser
 from .etl import transform, etl
 from .module import Module
-from .sym import net_viz, get_bp_loss
+from .sym import net_viz, get_bp_loss, net_init, fit_f
 
 __all__ = [
     "Module",
     "Configuration", "ConfigurationParser",
-    "net_viz", "get_bp_loss",
+    "net_viz", "get_bp_loss", "net_init", "fit_f",
     "etl", "transform"
 ]
