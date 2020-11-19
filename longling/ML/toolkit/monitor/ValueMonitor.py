06f6141f10b030a1b22c90adc4d84124cce961c4
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-19 11:18:55 +0800

    [feat] enable digits control and add func property

--- longling/ML/toolkit/monitor/ValueMonitor.py
+++ longling/ML/toolkit/monitor/ValueMonitor.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 # 2019/11/28 @ tongshiwei
 
-
 import math
 from collections import Iterable
 
@@ -35,8 +34,9 @@ class ValueMonitor(object):
     True
     """
 
-    def __init__(self, value_function_names: (list, dict), digits=None, *args, **kwargs):
+    def __init__(self, value_function_names: (list, dict), digits=6, *args, **kwargs):
         self._values = {name: NAN for name in value_function_names}
+        self._funcs = value_function_names if isinstance(value_function_names, dict) else {}
         self._digits = digits
 
     def __str__(self):
@@ -51,14 +51,21 @@ class ValueMonitor(object):
     def __contains__(self, name):
         return name in self._values
 
-    def monitor_on(self, name):
+    def monitor_on(self, name, func=None):
         self._values[name] = NAN
+        if func is not None:
+            self._funcs[name] = func
 
     def monitor_off(self, name):
         del self._values[name]
+        if name in self._funcs:
+            del self._funcs[name]
 
-    def update(self, *args, **kwargs):
+    def get_update_value(self, name, value):
         raise NotImplementedError
+
+    def update(self, name, value):
+        self._values[name] = self.round(self.get_update_value(name, value))
 
     def reset(self):
         for name in self._values:
@@ -74,11 +81,18 @@ class ValueMonitor(object):
         return self._values.keys()
 
     @property
+    def func(self):
+        return self._funcs
+
+    @property
     def value(self):
         if self._digits:
             return {key: round(value, self._digits) for key, value in self._values.items()}
         else:
             return self._values
+
+    def round(self, value):
+        return round(value, self._digits) if self._digits else value
 
 
 class EMAValue(ValueMonitor):
@@ -116,7 +130,7 @@ class EMAValue(ValueMonitor):
     10.9
     """
 
-    def __init__(self, value_function_names: (list, dict), smoothing_constant=0.01):
+    def __init__(self, value_function_names: (list, dict), smoothing_constant=0.01, *args, **kwargs):
         """
         Parameters
         ----------
@@ -125,10 +139,10 @@ class EMAValue(ValueMonitor):
             Smoothing factor in range [0, 1], [default: 0.01].
             Increase to give more weight to recent values.
         """
-        super(EMAValue, self).__init__(value_function_names)
+        super(EMAValue, self).__init__(value_function_names, *args, **kwargs)
         self.smoothing_constant = smoothing_constant
 
-    def update(self, name: str, value: (float, int)):
+    def get_update_value(self, name: str, value: (float, int)):
         """
         Parameters
         ----------
@@ -137,7 +151,7 @@ class EMAValue(ValueMonitor):
         value  : int or float
             New value to include in EMA.
         """
-        self._values[name] = (
+        return (
             value if math.isnan(self._values[name])
             else (1 - self.smoothing_constant) * self._values[
                 name] + self.smoothing_constant * value
