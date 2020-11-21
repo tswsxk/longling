7c8da4700d42d1dae1ba2e72533026efbf2810ee
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-21 12:40:13 +0800

    [feat] add pair_sampler

--- longling/ML/toolkit/dataset/__init__.py
+++ longling/ML/toolkit/dataset/__init__.py
@@ -7,4 +7,6 @@
 # from .splitter.iterable_splitter import *
 # from .splitter.splitter import Splitter
 
+from .feature import ID2Feature
+from .sampler import *
 from .split import train_test, train_valid_test, kfold
