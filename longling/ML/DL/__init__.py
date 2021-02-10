ad367ed941425de25abf8757a4668ef14f17a89c
Sherlcok <tongsw@mail.ustc.edu.cn>
2021-02-10 16:46:53 +0800

    [feat] add light_module

--- longling/ML/DL/__init__.py
+++ longling/ML/DL/__init__.py
@@ -5,3 +5,4 @@ from .ecli import cli as ecli
 from .module import Module
 from .optimizer_cfg import get_optimizer_cfg
 from .service import CliServiceModule, service_wrapper
+from .light_module import train
---
+++ longling/ML/DL/light_module.py
@@ -0,0 +1,115 @@
+# coding: utf-8
+# 2021/2/10 @ tongshiwei
+
+from longling import path_append
+from longling.ML.toolkit.hyper_search import prepare_hyper_search
+from longling.ML.toolkit import EpochEvalFMT as Formatter
+from longling.ML.toolkit import MovingLoss
+from tqdm import tqdm
+from longling.ML import get_epoch_params_filepath
+
+
+def train(
+        net, cfg, loss_function, trainer,
+        train_data, test_data=None,
+        params_save=False,
+        *,
+        fit_f, eval_f=None, net_init=None, get_net=None, get_loss=None, get_trainer=None, save_params=None,
+        reporthook=None, final_reporthook=None, primary_key=None,
+        **cfg_kwargs):
+    net = net if get_net is None else get_net(**cfg.hyper_params)
+
+    if net_init is not None:
+        net_init(net, cfg=cfg, **cfg.init_params)
+
+    cfg_kwargs, reporthook, final_reporthook, tag = prepare_hyper_search(
+        cfg_kwargs, reporthook, final_reporthook, primary_key=primary_key, with_keys="Epoch"
+    )
+
+    ctx = cfg.ctx
+    batch_size = cfg.batch_size
+
+    loss_function = get_loss(**cfg.loss_params) if get_loss is not None else loss_function
+
+    loss_monitor = MovingLoss(loss_function)
+    progress_monitor = tqdm
+    dump_result = not tag
+    if dump_result:
+        from longling import config_logging
+        validation_logger = config_logging(
+            filename=path_append(cfg.model_dir, "result.log"),
+            logger="%s-validation" % cfg.model_name,
+            mode="w",
+            log_format="%(message)s",
+        )
+        evaluation_formatter = Formatter(
+            logger=validation_logger,
+            dump_file=cfg.validation_result_file,
+        )
+    else:
+        evaluation_formatter = Formatter()
+
+    # train check
+    trainer = get_trainer(
+        net, optimizer=cfg.optimizer,
+        optimizer_params=cfg.optimizer_params,
+        select=cfg.train_select,
+        lr_params=cfg.lr_params
+    ) if get_trainer is not None else trainer
+
+    for epoch in range(cfg.begin_epoch, cfg.end_epoch):
+        for i, batch_data in enumerate(progress_monitor(train_data, "Epoch: %s" % epoch)):
+            fit_f(
+                net=net, batch_size=batch_size, batch_data=batch_data,
+                trainer=trainer,
+                loss_function=loss_function,
+                loss_monitor=loss_monitor,
+                ctx=ctx,
+            )
+
+        if cfg.lr_params and "update_params" in cfg.lr_params and cfg.end_epoch - cfg.begin_epoch - 1 > 0:
+            cfg.logger.info("reset trainer")
+            lr_params = cfg.lr_params.pop("update_params")
+            lr_update_params = dict(
+                batches_per_epoch=i + 1,
+                lr=cfg.optimizer_params["learning_rate"],
+                update_epoch=lr_params.get(
+                    "update_epoch",
+                    cfg.end_epoch - cfg.begin_epoch - 1
+                )
+            )
+            lr_update_params.update(lr_params)
+
+            assert get_trainer is not None
+            trainer = get_trainer(
+                net, optimizer=cfg.optimizer,
+                optimizer_params=cfg.optimizer_params,
+                lr_params=lr_update_params,
+                select=cfg.train_select,
+                logger=cfg.logger
+            )
+
+        if epoch % 1 == 0:
+            msg, data = evaluation_formatter(
+                iteration=epoch,
+                loss_name_value=dict(loss_monitor.items()),
+                eval_name_value=eval_f(net, test_data, ctx=ctx),
+                extra_info=None,
+                dump=dump_result,
+                keep={"msg", "data"}
+            )
+            print(msg)
+            if reporthook is not None:
+                reporthook(data)
+
+        # optional
+        loss_monitor.reset()
+
+        if params_save and (epoch % cfg.save_epoch == 0 or epoch == cfg.end_epoch - 1):
+            assert save_params is not None
+            params_path = get_epoch_params_filepath(cfg.model_name, epoch, cfg.model_dir)
+            cfg.logger.info("save model params to %s, with select='%s'" % (params_path, cfg.save_select))
+            save_params(params_path, net, select=cfg.save_select)
+
+    if final_reporthook is not None:
+        final_reporthook()
