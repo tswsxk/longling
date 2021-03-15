8acf6377dad231842923691b178341a0b91fd12e
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-03-15 22:00:32 +0800

    [feat] export all functions in toolkit

--- longling/ML/PytorchHelper/__init__.py
+++ longling/ML/PytorchHelper/__init__.py
@@ -2,5 +2,4 @@
 # create by tongshiwei on 2019-8-16
 
 from .helper import pick, tensor2list
-from .util.sequence import sequence_mask
-from .toolkit.parallel import set_device
+from .toolkit import *
