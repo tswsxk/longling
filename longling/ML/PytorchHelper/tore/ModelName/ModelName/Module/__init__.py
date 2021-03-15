96c8dc94b2130726d67896a166a6f7c89f1dd625
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-03-15 17:18:13 +0800

    [feat] export fit_f, net_init

--- longling/ML/PytorchHelper/tore/ModelName/ModelName/Module/__init__.py
+++ longling/ML/PytorchHelper/tore/ModelName/ModelName/Module/__init__.py
@@ -5,11 +5,11 @@ from __future__ import absolute_import
 from .configuration import Configuration, ConfigurationParser
 from .etl import transform, etl
 from .module import Module
-from .sym import get_bp_loss
+from .sym import get_bp_loss, fit_f, net_init
 
 __all__ = [
     "Module",
     "Configuration", "ConfigurationParser",
     "get_bp_loss",
-    "etl", "transform"
+    "etl", "transform", "fit_f", "net_init"
 ]
