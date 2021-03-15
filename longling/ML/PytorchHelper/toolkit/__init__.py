c2cd75b1bd10c5bb5b2a5d00630841cf6ee0e8b4
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-03-15 21:56:58 +0800

    [feat] export toolkit functions

--- longling/ML/PytorchHelper/toolkit/__init__.py
+++ longling/ML/PytorchHelper/toolkit/__init__.py
@@ -1,2 +1,12 @@
 # coding: utf-8
 # create by tongshiwei on 2019-8-30
+
+from .trainer import get_trainer
+from .optimizer_cfg import get_optimizer_cfg
+from .loss import loss_dict2tmt_torch_loss
+from .init import net_initialize, load_net
+from .fit import fit_wrapper
+from .eval import eval_wrapper
+from .parallel import set_device
+from .persistence import save_params
+from .lr_scheduler import get_lr_scheduler
