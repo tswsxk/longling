22c63ccf44f8167485072801d2b0bf942c431158
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-06-30 21:12:23 +0800

    [feat] remove tmt_value

--- longling/ML/toolkit/monitor/__init__.py
+++ longling/ML/toolkit/monitor/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # create by tongshiwei on 2019/4/7
 
-from .ValueMonitor import EMAValue, tmt_value, as_tmt_value
-from .LossMonitor import MovingLoss, tmt_loss, as_tmt_loss
+from .ValueMonitor import EMAValue, as_tmt_value
+from .LossMonitor import MovingLoss, as_tmt_loss
 from .ProgressMonitor import ConsoleProgressMonitor
