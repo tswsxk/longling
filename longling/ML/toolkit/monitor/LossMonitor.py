432735f11d855c60207f1110569d3a641597667f
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-07-01 10:59:22 +0800

    [docs] add examples

--- longling/ML/toolkit/monitor/LossMonitor.py
+++ longling/ML/toolkit/monitor/LossMonitor.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 # create by tongshiwei on 2018/7/14
 
-from .ValueMonitor import ValueMonitor, EMAValue, as_tmt_value
-
+# from .ValueMonitor import ValueMonitor, EMAValue, as_tmt_value
+from longling import as_list
+from longling.ML.toolkit.monitor.ValueMonitor import ValueMonitor, EMAValue, as_tmt_value
 __all__ = ["LossMonitor", "MovingLoss", "as_tmt_loss", "loss_dict2tmt_loss"]
 
 
@@ -11,13 +12,79 @@ __all__ = ["LossMonitor", "MovingLoss", "as_tmt_loss", "loss_dict2tmt_loss"]
 
 
 def as_tmt_loss(loss_obj, loss2value=lambda x: x):
+    """
+
+    Parameters
+    ----------
+    loss_obj
+    loss2value
+
+    Returns
+    -------
+
+    Examples
+    --------
+    >>> @as_tmt_loss
+    ... def mse(v):
+    ...     return v ** 2
+    >>> mse(2)
+    4
+    """
     return as_tmt_value(loss_obj, loss2value)
 
 
 def loss_dict2tmt_loss(loss_dict, loss2value=lambda x: x, exclude=None, include=None, as_loss=as_tmt_loss):
-    exclude = set() if exclude is None else set(exclude)
+    """
+
+    Parameters
+    ----------
+    loss_dict
+    loss2value
+    exclude
+    include
+    as_loss
+
+    Returns
+    -------
+
+    Examples
+    --------
+    >>> def mse(v):
+    ...     return v ** 2
+    >>> losses = loss_dict2tmt_loss({"mse": mse, "rmse": lambda x: x})
+    >>> losses.keys()
+    dict_keys(['mse', 'rmse'])
+    >>> ema = EMAValue(losses)
+    >>> losses["mse"](2)
+    4
+    >>> losses["rmse"](2)
+    2
+    >>> ema.items()
+    dict_items([('mse', 4), ('rmse', 2)])
+    >>> losses = loss_dict2tmt_loss({"mse": mse, "rmse": lambda x: x}, include="mse")
+    >>> losses.keys()
+    dict_keys(['mse', 'rmse'])
+    >>> ema = EMAValue(losses)
+    >>> losses["mse"](2)
+    4
+    >>> losses["rmse"](2)
+    2
+    >>> ema.items()
+    dict_items([('mse', 4), ('rmse', nan)])
+    >>> losses = loss_dict2tmt_loss({"mse": mse, "rmse": lambda x: x}, exclude="mse")
+    >>> losses.keys()
+    dict_keys(['mse', 'rmse'])
+    >>> ema = EMAValue(losses)
+    >>> losses["mse"](2)
+    4
+    >>> losses["rmse"](2)
+    2
+    >>> ema.items()
+    dict_items([('mse', nan), ('rmse', 2)])
+    """
+    exclude = set() if exclude is None else set(as_list(exclude))
     if include is not None:
-        include = set(include)
+        include = set(as_list(include))
         return {
             name: as_loss(func, loss2value) if name in include else func for name, func in loss_dict.items()
         }
