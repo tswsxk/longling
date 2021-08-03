e14c00189196816ec77841cfcc00a5365293d348
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-03 19:38:57 +0800

    [chore] update classifier information

--- setup.py
+++ setup.py
@@ -165,8 +165,10 @@ setup(
         "full": full_deps
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
