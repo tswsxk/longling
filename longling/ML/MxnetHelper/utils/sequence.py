3a60312509751e15bfb364ca1524864e1b16b81f
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-06 23:12:33 +0800

    [docs] add mxnet.symbol examples

--- longling/ML/MxnetHelper/utils/sequence.py
+++ longling/ML/MxnetHelper/utils/sequence.py
@@ -55,6 +55,26 @@ def format_sequence(length, inputs, layout, merge, in_layout=None):
       [3. 3. 3. 3.]
       [5. 5. 5. 5.]]]
     <NDArray 2x3x4 @cpu(0)>
+    >>> import mxnet.symbol as sym
+    >>> seq3, _, _, _ = format_sequence(3, sym.Variable("s", shape=(2, 3, 4)), "NTC", False)
+    >>> seq3
+    [<Symbol split0>, <Symbol split0>, <Symbol split0>]
+    >>> seq4 = [nd.array([[0] * 4, [1] * 4]), nd.array([[2] * 4, [3] * 4]), nd.array([[4] * 4, [5] * 4])]
+    >>> seq5, _, _, _ = format_sequence(3, seq4, "NTC", True)
+    >>> seq5   # doctest: +NORMALIZE_WHITESPACE
+    <BLANKLINE>
+    [[[0. 0. 0. 0.]
+      [2. 2. 2. 2.]
+      [4. 4. 4. 4.]]
+    <BLANKLINE>
+     [[1. 1. 1. 1.]
+      [3. 3. 3. 3.]
+      [5. 5. 5. 5.]]]
+    <NDArray 2x3x4 @cpu(0)>
+    >>> seq6 = [sym.Variable("1", shape=(2, 4)), sym.Variable("2", shape=(2, 4)), sym.Variable("3", shape=(2, 4))]
+    >>> seq7, _, _, _ = format_sequence(3, seq6, "NTC", True)
+    >>> seq7
+    <Symbol stack0>
     """
     assert inputs is not None, \
         "unroll(inputs=None) has been deprecated. " \
@@ -94,7 +114,8 @@ def format_sequence(length, inputs, layout, merge, in_layout=None):
             inputs = F.stack(*inputs, axis=axis)
             in_axis = axis
 
-    if isinstance(inputs, tensor_types) and axis != in_axis:
+    if isinstance(inputs, tensor_types) and axis != in_axis:  # pragma: no cover
+        # todo: find the test case
         inputs = F.swapaxes(inputs, dim1=axis, dim2=in_axis)
 
     return inputs, axis, F, batch_size
@@ -153,6 +174,21 @@ def mask_sequence_variable_length(F, data, length, valid_length, time_axis,
       [1. 1. 1.]
       [1. 1. 1.]]]
     <NDArray 2x4x3 @cpu(0)>
+    >>> mask_sequence_variable_length(
+    ...     nd, [mx.nd.ones((2, 3)), mx.nd.ones((2, 3)), mx.nd.ones((2, 3)), mx.nd.ones((2, 3))],
+    ...     4, nd.array([2, 4]), 1, True
+    ... )
+    <BLANKLINE>
+    [[[1. 1. 1.]
+      [1. 1. 1.]
+      [0. 0. 0.]
+      [0. 0. 0.]]
+    <BLANKLINE>
+     [[1. 1. 1.]
+      [1. 1. 1.]
+      [1. 1. 1.]
+      [1. 1. 1.]]]
+    <NDArray 2x4x3 @cpu(0)>
     """
     assert valid_length is not None
     if not isinstance(data, tensor_types):
@@ -196,6 +232,10 @@ def get_begin_state(cell, F, begin_state, inputs, batch_size):
     [[0. 0. 0.]
      [0. 0. 0.]]
     <NDArray 2x3 @cpu(0)>]
+    >>> import mxnet.symbol as sym
+    >>> get_begin_state(lstm_cell, sym, None,
+    ...     sym.Variable("inputs", shape=(2, 4, 5)), 2)   # doctest: +NORMALIZE_WHITESPACE
+    [<Symbol lstm0_begin_state_2>, <Symbol lstm0_begin_state_3>]
     """
     if begin_state is None:
         if F is ndarray:
