06d2ac4dc8215fa384579b412438118593ea5e2f
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-06 23:11:54 +0800

    [refactor] remove and add some functions [docs] add examples and argument annotations

--- longling/ML/DL/utils/__init__.py
+++ longling/ML/DL/utils/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 # 2021/3/15 @ tongshiwei
 
-from .lr_scheduler import get_total_update_steps, get_factor_lr_params, _stop_lr as stop_lr
+from .lr_scheduler import get_max_update, get_lr_factor, get_final_lr, get_step
 from .select import *
