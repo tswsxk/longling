99d5a92f266d2426803ce01f702a0597fee7cd50
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-09-03 15:26:27 +0800

    [fix] change test cases

--- tests/test_spider/test_parser.py
+++ tests/test_spider/test_parser.py
@@ -8,13 +8,13 @@ from longling.spider.parser import lxml
 
 
 def test_lxml_parser():
-    url = "http://base.ustc.edu.cn/"
+    url = "http://bigdata.ustc.edu.cn/"
     r = requests.get(url, timeout=30)
     r.raise_for_status()
     r.encoding = r.apparent_encoding
     lxml_text = BeautifulSoup(r.text, "lxml")
 
-    assert "http://bigdata.ustc.edu.cn/" in set(lxml.get_all_url(lxml_text))
+    assert "http://www.ustc.edu.cn" in set(lxml.get_all_url(lxml_text))
 
     lxml.get_all_text(lxml_text)
 
